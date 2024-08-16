d2a49bffc86aab1e585c64315537abad896ac412
laxyy69 <laxyy420@proton.me>
2024-08-16 14:04:07 +0200

    test_client: Use the new segment buffering and serialization

--- tests/client.c
+++ tests/client.c
@@ -1,19 +1,60 @@
 #include "ssp.h"
 #include "ssp_tcp.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 i32 
 main(void)
 {
-    const char* ipaddr = "127.0.0.1";
-    const char* msg = "SSP - Simple Segmented Protocol";
+    const char* ipaddr = "";
+    const char* msg1 = "SSP - Simple Segmented Protocol";
+    const char* msg2 = "Testing Some Stuff";
     u16 port = 8080;
     ssp_tcp_sock_t sock;
+    ssp_segbuff_t segbuf;
+    u32 len1 = strlen(msg1);
+    u32 len2 = strlen(msg2);
+    ssp_packet_t* packet;
+    printf("sizeof segment: %lu\n", sizeof(ssp_segment_t));
 
     ssp_tcp_sock_create(&sock, SSP_IPv4);
     if (ssp_tcp_connect(&sock, ipaddr, port) == -1)
         return -1;
+    ssp_segbuff_init(&segbuf, 10);
 
-    ssp_tcp_send_msg(&sock, msg);
+    // Add msg type 6969 & CCCC
+    ssp_segbuff_add(&segbuf, 0x6969, len1, msg1);
+    ssp_segbuff_add(&segbuf, 0xCCCC, len2, msg2);
+
+    /**
+     * Serialize packet from segbuf, send it, and free packet
+     * Expect: Send `msg` twice.
+     */
+    packet = ssp_serialize_packet(&segbuf);
+    ssp_tcp_send(&sock, packet);
+    free(packet);
+
+    // Add msg as type EEEE
+    ssp_segbuff_add(&segbuf, 0xEEEE, len2, msg2);
+
+    /**
+     * Serialize packet from segbuf, send it, and free packet
+     * Expect: Send `msg` only once.
+     */
+    packet = ssp_serialize_packet(&segbuf);
+    ssp_tcp_send(&sock, packet);
+    free(packet);
+
+    /**
+     * This time it should not work
+     * `ssp_serialize_packet()` should return NULL
+     * because ssp_serialize_packet() clears `segbuf`
+     */
+    packet = ssp_serialize_packet(&segbuf);
+    ssp_tcp_send(&sock, packet);
+    free(packet);
+
     ssp_tcp_sock_close(&sock);
 
     return 0;
